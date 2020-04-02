b7fd519939381566c2d5791c7d6563b52f0619e3
ayush <ayushsinha126@gmail.com>
2020-04-02 20:07:30 +0530

    updated controller.js

--- src/components/upload/controller.js
+++ src/components/upload/controller.js
@@ -45,36 +45,3 @@ exports.corona_data = (req,res)=>{
     });
 };
 
-/*
-exports.up = (req,res)=>{
-    covid_data_schema.deleteMany({}).then(()=>{
-
-        axios.get(`https://api.covid19india.org/data.json`).then((reply)=>{
-
-        covid_data = reply.data.statewise;
-        covid_data.forEach(element => {
-            const covid = new covid_data_schema({
-                uid: shortid.generate(),
-                state: element.state,
-                active: element.active,
-                confirmed: element.confirmed,
-                deaths: element.deaths,
-                recovered: element.recovered
-            });
-            covid.save().then(()=>{
-                console.log('data saved');
-            }).catch((e)=>{
-                console.log(`${e}`);
-            });
-        });
-
-        next();
-        
-        }).catch((e)=>{
-            console.log(`${e}`);
-        });
-
-    }).catch((e)=>{
-        console.log(`Error in deleting the documents -> ${e}`);
-    });
-};*/
